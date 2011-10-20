362e266b6125d6792ad1a30559d335678e90d99a
John Arbash Meinel <john@arbash-meinel.com>
2011-10-20 13:10:24 +0200

    Implement client request tracking in the server object.

--- u1db/remote_sync_server.py
+++ u1db/remote_sync_server.py
@@ -14,6 +14,64 @@
 
 """A Server that listens for synchronization requests"""
 
+import threading
+import SocketServer
+
+
+class TCPSyncServer(SocketServer.TCPServer):
+
+    allow_reuse_address = False # Should be set to True for testing
+    daemon_threads = False
+
+    def __init__(self, server_address, RequestHandlerClass):
+        SocketServer.TCPServer.__init__(self, server_address,
+                                        RequestHandlerClass)
+        self._request_threads = {}
+        self._request_threads_lock = threading.Lock()
+
+    def process_request_thread(self, request, client_address):
+        """Same as in BaseServer but as a thread.
+
+        In addition, exception handling is done here.
+
+        """
+        try:
+            self.finish_request(request, client_address)
+            self.close_request(request)
+        except:
+            self.handle_error(request, client_address)
+            self.close_request(request)
+
+    def _add_request(self, request, t):
+        with self._request_threads_lock:
+            self._request_threads[request] = t
+
+    def _remove_request(self, request):
+        with self._request_threads_lock:
+            if request in self._request_threads:
+                t = self._request_threads[request]
+                return t
+        return None
+
+    def process_request(self, request, client_address):
+        """Start a new thread to process the request."""
+        t = threading.Thread(target = self.process_request_thread,
+                             args = (request, client_address))
+        self._add_request(request, t)
+        if self.daemon_threads:
+            t.setDaemon (1)
+        t.start()
+
+    def close_request(self, request):
+        SocketServer.TCPServer.close_request(self, request)
+        return self._remove_request(request)
+
+
+class TCPSyncRequestHandler(SocketServer.StreamRequestHandler):
+
+    def handle(self):
+        pass
+
 
 class RemoteSyncServer(object):
     """Listen for requests to synchronize."""
--- u1db/tests/test_remote_sync_server.py
+++ u1db/tests/test_remote_sync_server.py
@@ -14,6 +14,10 @@
 
 """Tests for the remote synchronization server"""
 
+import threading
+import socket
+import SocketServer
+
 from u1db import (
     errors,
     remote_sync_server,
@@ -25,7 +29,63 @@ from u1db.backends import inmemory
 class TestRemoteSyncServer(tests.TestCase):
 
     def test_takes_database(self):
-        db =  inmemory.InMemoryDatabase('test')
+        db = inmemory.InMemoryDatabase('test')
         server = remote_sync_server.RemoteSyncServer(db)
 
 
+class MyHelloHandler(SocketServer.StreamRequestHandler):
+
+    def handle(self):
+        value = self.rfile.readline()
+        if value == 'hello\n':
+            self.wfile.write('hello to you, too\n')
+
+
+class TwoMessageHandler(SocketServer.StreamRequestHandler):
+
+    def handle(self):
+        value = self.rfile.readline()
+        self.wfile.write('same to you\n')
+        value = self.rfile.readline()
+        self.wfile.write('goodbye\n')
+
+
+class TestTCPSyncServer(tests.TestCase):
+
+    def startServer(self, request_handler):
+        self.server = remote_sync_server.TCPSyncServer(
+            ('127.0.0.1', 0), request_handler)
+        self.server_thread = threading.Thread(target=self.server.serve_forever,
+                                              kwargs=dict(poll_interval=0.01))
+        self.server_thread.start()
+        # Because TCPSyncServer sets bind_and_activate=True in its constructor,
+        # the socket is already created. So the client can bind, even if the
+        # server thread isn't actively listening yet.
+        self.addCleanup(self.server_thread.join)
+        self.addCleanup(self.server.shutdown)
+
+    def connectToServer(self):
+        client_sock = socket.socket()
+        client_sock.connect(self.server.server_address)
+        return client_sock
+
+    def test_start_and_stop_server_in_a_thread(self):
+        self.startServer(remote_sync_server.TCPSyncRequestHandler)
+        self.server.shutdown()
+        self.server_thread.join()
+
+    def test_say_hello(self):
+        self.startServer(MyHelloHandler)
+        client_sock = self.connectToServer()
+        client_sock.sendall('hello\n')
+        self.assertEqual('hello to you, too\n', client_sock.recv(1024))
+        client_sock.close()
+
+    def test_tracks_active_connections(self):
+        self.startServer(TwoMessageHandler)
+        client_sock = self.connectToServer()
+        client_sock.sendall('hello\n')
+        self.assertEqual('same to you\n', client_sock.recv(1024))
+        self.assertEqual(1, len(self.server._request_threads))
+        client_sock.sendall('goodbye\n')
+        self.assertEqual('goodbye\n', client_sock.recv(1024))
