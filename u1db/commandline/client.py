f8a96d3b1fe63153198bc0ed4502337909808828
John Arbash Meinel <john@arbash-meinel.com>
2011-10-31 23:44:10 +0100

    Start implementing client commandline functionality.
    This implements get and create, soon to implement put and sync.

--- u1db/commandline/client.py
+++ u1db/commandline/client.py
@@ -20,17 +20,43 @@ import sys
 from u1db import (
     __version__ as _u1db_version,
     )
+from u1db.backends import sqlite_backend
 from u1db.remote import (
     client,
     )
 
 
-def client_get(args):
-    """Run 'get' for this client"""
+def cmd_create(database, doc_id, in_file, out_file):
+    """Run 'create_doc'."""
+    db = sqlite_backend.SQLiteDatabase.open_database(database)
+    doc_id, doc_rev = db.create_doc(in_file.read(), doc_id=doc_id)
+    out_file.write('doc_id: %s\ndoc_rev: %s\n' % (doc_id, doc_rev))
 
+
+def client_create(args):
+    return cmd_create(args.database, args.doc_id, args.infile, sys.stdout)
+
+
+def cmd_get(database, doc_id, out_file, err_file):
+    """actually run 'get_doc' on the given parameters"""
+    db = sqlite_backend.SQLiteDatabase.open_database(database)
+    doc_rev, doc, has_conflicts = db.get_doc(doc_id)
+    out_file.write(doc)
+    err_file.write('doc_rev: %s\n' % (doc_rev,))
+    if has_conflicts:
+        pass
+
+
+def client_get(args):
+    """Run 'get_doc' for this client"""
+    return do_get(args.database, args.doc_id, args.outfile, sys.stderr)
+
+
+def cmd_put(database, doc_id, old_doc_rev, in_file, out_file):
+    """run 'put_doc' and update the data."""
 
 def client_put(args):
-    """Run 'put'"""
+    """Run 'put_doc'"""
 
 
 def client_sync(args):
@@ -42,6 +68,14 @@ def setup_arg_parser():
     p.add_argument('-V', '--version', action='version', version=_u1db_version)
     p.add_argument('-v', '--verbose', action='store_true', help='be chatty')
     subs = p.add_subparsers(title='subcommands')
+    parser_create = subs.add_parser('create',
+        help='Create a new document from scratch')
+    parser_create.add_argument('database', help='The database to update')
+    parser_create.add_argument('infile', nargs='?', default=sys.stdin,
+        help='The file to read content from.')
+    parser_create.add_argument('--doc-id', default=None,
+        help='Set the document identifier')
+    parser_create.set_defaults(func=client_create)
     parser_get = subs.add_parser('get',
         help='Extract a document from the database')
     parser_get.add_argument('database', help='The database to query')
@@ -51,8 +85,8 @@ def setup_arg_parser():
         type=argparse.FileType('wb'))
     parser_get.set_defaults(func=client_get)
     parser_put = subs.add_parser('put', help='Add a document to the database')
-    parser_put.add_argument('database', help='The database to query')
-    parser_put.add_argument('doc_id', help='The document id to retrieve.')
+    parser_put.add_argument('database', help='The database to update')
+    parser_put.add_argument('doc_id', help='The document id to retrieve')
     parser_put.add_argument('doc_rev',
         help='The revision of the document (which is being superseded.)')
     parser_put.add_argument('infile', nargs='?', default=sys.stdin,
@@ -69,3 +103,4 @@ def setup_arg_parser():
 def main(args):
     p = setup_arg_parser()
     args = p.parse_args(args)
+    return args.func(args)
--- u1db/tests/commandline/test_client.py
+++ u1db/tests/commandline/test_client.py
@@ -12,6 +12,8 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import cStringIO
+import os
 import sys
 import subprocess
 import time
@@ -20,7 +22,11 @@ from u1db import (
     __version__ as _u1db_version,
     tests,
     )
+from u1db.backends import (
+    sqlite_backend,
+    )
 from u1db.commandline import client
+from u1db.tests.commandline import safe_close
 
 
 class TestArgs(tests.TestCase):
@@ -30,10 +36,28 @@ class TestArgs(tests.TestCase):
         self.parser = client.setup_arg_parser()
 
     def parse_args(self, args):
+        # ArgumentParser.parse_args doesn't play very nicely with a test suite,
+        # so we trap SystemExit in case something is wrong with the args we're
+        # parsing.
         try:
             return self.parser.parse_args(args)
         except SystemExit, e:
             raise AssertionError('got SystemExit')
+
+
+    def test_create(self):
+        args = self.parse_args(['create', 'test.db'])
+        self.assertEqual(client.client_create, args.func)
+        self.assertEqual('test.db', args.database)
+        self.assertEqual(None, args.doc_id)
+        self.assertEqual(sys.stdin, args.infile)
+
+    def test_create_custom_doc_id(self):
+        args = self.parse_args(['create', '--doc-id', 'xyz', 'test.db'])
+        self.assertEqual(client.client_create, args.func)
+        self.assertEqual('test.db', args.database)
+        self.assertEqual('xyz', args.doc_id)
+        self.assertEqual(sys.stdin, args.infile)
 
     def test_get(self):
         args = self.parse_args(['get', 'test.db', 'doc-id'])
@@ -62,3 +86,69 @@ class TestArgs(tests.TestCase):
         self.assertEqual(client.client_sync, args.func)
         self.assertEqual('source', args.source)
         self.assertEqual('target', args.target)
+
+
+class TestCaseWithDB(tests.TestCase):
+
+    def setUp(self):
+        super(TestCaseWithDB, self).setUp()
+        self.working_dir = self.createTempDir()
+        self.db_path = self.working_dir + '/test.db'
+        self.db = sqlite_backend.SQLitePartialExpandDatabase(self.db_path)
+        self.db._set_replica_uid('test')
+
+
+class TestCreate(TestCaseWithDB):
+
+    def test_create(self):
+        out = cStringIO.StringIO()
+        inf = cStringIO.StringIO(tests.simple_doc)
+        client.cmd_create(self.db_path, 'test-id', inf, out)
+        doc_rev, doc, has_conflicts = self.db.get_doc('test-id')
+        self.assertEqual(tests.simple_doc, doc)
+        self.assertFalse(has_conflicts)
+        self.assertEqual('doc_id: test-id\ndoc_rev: %s\n' % (doc_rev,),
+                         out.getvalue())
+
+
+class TestGet(TestCaseWithDB):
+
+    def setUp(self):
+        super(TestGet, self).setUp()
+        _, self.doc_rev = self.db.create_doc(tests.simple_doc,
+                                             doc_id='my-test-doc')
+
+    def test_get_simple(self):
+        out = cStringIO.StringIO()
+        err = cStringIO.StringIO()
+        client.cmd_get(self.db_path, 'my-test-doc', out_file=out, err_file=err)
+        self.assertEqual(tests.simple_doc, out.getvalue())
+        self.assertEqual('doc_rev: %s\n' % (self.doc_rev,),
+                         err.getvalue())
+
+
+class TestCommandLine(TestCaseWithDB):
+
+    def _get_u1db_client_path(self):
+        from u1db import __path__ as u1db_path
+        u1db_parent_dir = os.path.dirname(u1db_path[0])
+        return os.path.join(u1db_parent_dir, 'u1db-client')
+
+    def runU1DBClient(self, args):
+        command = [sys.executable, self._get_u1db_client_path()]
+        command.extend(args)
+        p = subprocess.Popen(command, stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.addCleanup(safe_close, p)
+        return p
+
+    def test_create(self):
+        p = self.runU1DBClient(['create', '--doc-id', 'test-id', self.db_path])
+        stdout, stderr = p.communicate(tests.simple_doc)
+        self.assertEqual(0, p.returncode)
+        self.assertEqual('', stderr)
+        doc_rev, doc, has_conflicts = self.db.get_doc('test-id')
+        self.assertEqual(tests.simple_doc, doc)
+        self.assertFalse(has_conflicts)
+        self.assertEqual('doc_id: test-id\ndoc_rev: %s\n' % (doc_rev,),
+                         stdout)
