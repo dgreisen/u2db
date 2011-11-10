49e804162215f998dc6d306b5755746638f359b2
Samuele Pedroni (Canonical Services Ltd.) <samuele.pedroni@canonical.com>
2011-11-10 20:25:44 +0100

     start writing a wsgi app to access u1db databases

---
+++ u1db/remote/http_app.py
@@ -0,0 +1,181 @@
+# Copyright 2011 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""HTTP Application exposing U1DB."""
+import httplib
+import json
+import urlparse
+
+
+class Doc(object):
+    """Document resource."""
+
+    def __init__(self, dbname, id, state, responder):
+        self.id = id
+        self.responder = responder
+        self.db = state.open_database(dbname)
+
+    def put(self, body, args):
+        doc_rev = self.db.put_doc(self.id, None, body)
+        self.responder.send_response(rev=doc_rev) # xxx some other 20x status
+
+
+class Sync(object):
+    """Sync endpoint resource."""
+
+    def __init__(self, dbname, from_replica_uid, state, responder):
+        self.from_replica_uid = from_replica_uid
+        self.responder = responder
+        self.target = state.open_database(dbname).get_sync_target()
+
+    def get(self, args):
+        result = self.target.get_sync_info(self.from_replica_uid)
+        self.responder.send_response(this_replica_uid=result[0],
+                                     this_replica_generation=result[1],
+                                     other_replica_uid=self.from_replica_uid,
+                                     other_replica_generation=result[2])
+
+    def put(self, body, args): # xxx ask for the upper layers to unserialize
+        data = json.loads(body)
+        self.target.record_sync_info(self.from_replica_uid,
+                                     data['generation'])
+        self.responder.send_response(ok=True)
+
+    # Implements the same logic as LocalSyncTarget.sync_exchange
+
+    def post_args(self, args):
+        self.from_replica_generation = args['from_replica_generation']
+        self.last_known_generation = args['last_known_generation']
+        self.sync_exch = self.target.get_sync_exchange()
+
+    def post_stream_entry(self, entry):
+        entry = json.loads(entry)
+        self.sync_exch.insert_doc_from_source(entry['id'], entry['rev'],
+                                              entry['doc'])
+
+    def post_end(self):
+        def send_doc(doc_id, doc_rev, doc):
+            entry = dict(id=doc_id, rev=doc_rev, doc=doc)
+            self.responder.stream_entry(entry)
+        new_gen = self.sync_exch.find_docs_to_return(self.last_known_generation)
+        self.responder.content_type = 'application/x-u1db-multi-json'
+        self.responder.start_response(new_generation=new_gen)
+        new_gen = self.sync_exch.return_docs_and_record_sync(
+                                                  self.from_replica_uid,
+                                                  self.from_replica_generation,
+                                                  send_doc)
+        self.responder.finish_response()
+
+
+OK = 200
+
+class HTTPResponder(object):
+    """Encode responses from the server back to the client."""
+
+    # a multi document response will put args and documents
+    # each on one line of the response body
+
+    def __init__(self, start_response):
+        self._started = False
+        self.sent_response = False
+        self._start_response = start_response
+        self._write = None
+        self.content_type = 'application/json'
+
+    def start_response(self, status=OK, **kwargs):
+        """start sending response: header and args."""
+        if self._started:
+            return
+        self._started = True
+        status_text = httplib.responses[status]
+        self._write = self._start_response('%d %s' % (status, status_text),
+                                         [('content-type', self.content_type)])
+        # xxx version in headers
+        if kwargs:
+            self._write(json.dumps(kwargs)+"\r\n")
+
+    def finish_response(self):
+        """finish sending response."""
+        self.sent_response = True
+
+    def send_response(self, status=OK, **kwargs):
+        """send and finish response in one go."""
+        self.start_response(status, **kwargs)
+        self.finish_response()
+
+    def stream_entry(self, entry):
+        "send stream entry as part of the response."
+        assert self._started
+        self._write(json.dumps(entry)+"\r\n")
+
+
+class HTTPInvocationByMethodWithBody(object):
+    """Invoke methods on a resource."""
+
+    def __init__(self, resource, environ):
+        self.resource = resource
+        self.environ = environ
+
+    def __call__(self):
+        args = urlparse.parse_qsl(self.environ['QUERY_STRING'],
+                                  strict_parsing=False)
+        args = dict((k.decode('utf-8'), v.decode('utf-8')) for k,v in args)
+        method = self.environ['REQUEST_METHOD'].lower()
+        if method in ('get', 'delete'):
+            meth = getattr(self.resource, method)
+            return meth(args)
+        else:
+            content_type = self.environ.get('CONTENT_TYPE')
+            if content_type == 'application/json':
+                meth = getattr(self.resource, method)
+                body = self.environ['wsgi.input'].read()
+                return meth(body, args)
+            elif content_type == 'application/x-u1db-multi-json':
+                meth_args = getattr(self.resource, '%s_args' % method)
+                meth_entry = getattr(self.resource, '%s_stream_entry' % method)
+                meth_end = getattr(self.resource, '%s_end' % method)
+                body_readline = self.environ['wsgi.input'].readline
+                args.update(json.loads(body_readline()))
+                meth_args(args)
+                while True:
+                    line = body_readline()
+                    if not line:
+                        break
+                    entry = line.strip()
+                    meth_entry(entry)
+                return meth_end()
+            else:
+                raise ValueError
+
+
+class HTTPApp(object):
+
+    def __init__(self, state):
+        self.state = state
+
+    def __call__(self, environ, start_response):
+        responder = HTTPResponder(start_response)
+        parts = environ['PATH_INFO'].split('/')
+        resource = None
+        # xxx proper dispatch logic
+        # xxx error handling
+        if len(parts) == 4 and parts[2] == 'doc':
+            resource = Doc(parts[1], parts[3], self.state, responder)
+        elif len(parts) == 4 and parts[2] == 'sync-from':
+            resource = Sync(parts[1], parts[3], self.state, responder)
+        if resource:
+            HTTPInvocationByMethodWithBody(resource, environ)()
+        else:
+            responder.send_response(400)
+        return []
---
+++ u1db/tests/test_http_app.py
@@ -0,0 +1,208 @@
+# Copyright 2011 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test the WSGI app."""
+
+import testtools
+import paste.fixture
+import json
+import StringIO
+
+from u1db import (
+    tests,
+    )
+
+from u1db.remote import (
+    http_app,
+    )
+
+
+class TestResource(object):
+
+    def get(self, args):
+        self.args = args
+        return 'Get'
+
+    def put(self, body, args):
+        self.body = body
+        self.args = args
+        return 'Put'
+
+    def put_args(self, args):
+        self.args = args
+        self.order = ['a']
+        self.entries = []
+
+    def put_stream_entry(self, entry):
+        self.entries.append(entry)
+        self.order.append('s')
+
+    def put_end(self):
+        self.order.append('e')
+        return "Put/end"
+
+class TestHTTPInvocationByMethodWithBody(testtools.TestCase):
+
+    def test_get(self):
+        resource = TestResource()
+        environ = {'QUERY_STRING': 'a=1&b=2', 'REQUEST_METHOD': 'GET'}
+        invoke = http_app.HTTPInvocationByMethodWithBody(resource, environ)
+        res = invoke()
+        self.assertEqual('Get', res)
+        self.assertEqual({'a': '1', 'b': '2'}, resource.args)
+
+    def test_put_json(self):
+        resource = TestResource()
+        environ = {'QUERY_STRING': 'a=1', 'REQUEST_METHOD': 'PUT',
+                   'wsgi.input': StringIO.StringIO('{"body": true}'),
+                   'CONTENT_TYPE': 'application/json'}
+        invoke = http_app.HTTPInvocationByMethodWithBody(resource, environ)
+        res = invoke()
+        self.assertEqual('Put', res)
+        self.assertEqual({'a': '1'}, resource.args)
+        self.assertEqual('{"body": true}', resource.body)
+
+    def test_put_multi_json(self):
+        resource = TestResource()
+        environ = {'QUERY_STRING': 'a=1', 'REQUEST_METHOD': 'PUT',
+                   'wsgi.input': StringIO.StringIO(
+                       '{"b": 2}\r\n'       # args
+                       '{"entry": "x"}\r\n' # stream entry
+                       '{"entry": "y"}\r\n' # stream entry
+                       ),
+                   'CONTENT_TYPE': 'application/x-u1db-multi-json'}
+        invoke = http_app.HTTPInvocationByMethodWithBody(resource, environ)
+        res = invoke()
+        self.assertEqual('Put/end', res)
+        self.assertEqual({'a': '1', 'b': 2}, resource.args)
+        self.assertEqual(['{"entry": "x"}', '{"entry": "y"}'], resource.entries)
+        self.assertEqual(['a', 's', 's', 'e'], resource.order)
+
+
+class TestHTTPResponder(testtools.TestCase):
+
+    def start_response(self, status, headers):
+        self.status = status
+        self.headers = dict(headers)
+        self.response_body = []
+        def write(data):
+            self.response_body.append(data)
+        return write
+
+    def test_send_response(self):
+        responder = http_app.HTTPResponder(self.start_response)
+        responder.send_response(value='success')
+        self.assertEqual('200 OK', self.status)
+        self.assertEqual({'content-type': 'application/json'}, self.headers)
+        self.assertEqual(['{"value": "success"}\r\n'], self.response_body)
+
+    def test_send_response_status_fail(self):
+        responder = http_app.HTTPResponder(self.start_response)
+        responder.send_response(400)
+        self.assertEqual('400 Bad Request', self.status)
+        self.assertEqual({'content-type': 'application/json'}, self.headers)
+        self.assertEqual([], self.response_body)
+
+    def test_start_finish_response_status_fail(self):
+        responder = http_app.HTTPResponder(self.start_response)
+        responder.start_response(404, error='not found')
+        responder.finish_response()
+        self.assertEqual('404 Not Found', self.status)
+        self.assertEqual({'content-type': 'application/json'}, self.headers)
+        self.assertEqual(['{"error": "not found"}\r\n'], self.response_body)
+
+    def test_send_stream_entry(self):
+        responder = http_app.HTTPResponder(self.start_response)
+        responder.content_type = "application/x-u1db-multi-json"
+        responder.start_response(one=1)
+        responder.stream_entry({'entry': True})
+        responder.finish_response()
+        self.assertEqual('200 OK', self.status)
+        self.assertEqual({'content-type': 'application/x-u1db-multi-json'},
+                         self.headers)
+        self.assertEqual(['{"one": 1}\r\n',
+                          '{"entry": true}\r\n'], self.response_body)
+
+
+class TestHTTPApp(testtools.TestCase):
+
+    def setUp(self):
+        super(TestHTTPApp, self).setUp()
+        self.state = tests.ServerStateForTests()
+        application = http_app.HTTPApp(self.state)
+        self.app = paste.fixture.TestApp(application)
+        self.db0 = self.state._create_database('db0')
+
+    def test_put_doc(self):
+        resp = self.app.put('/db0/doc/doc1', params='{"x": 1}',
+                            headers={'content-type': 'application/json'})
+        doc_rev, doc, _ = self.db0.get_doc('doc1')
+        self.assertEqual(200, resp.status)
+        self.assertEqual('{"x": 1}', doc)
+        self.assertEqual('application/json', resp.header('content-type'))
+        self.assertEqual({'rev': doc_rev}, json.loads(resp.body))
+
+    def test_get_sync_info(self):
+        self.db0.set_sync_generation('other-id', 1)
+        resp = self.app.get('/db0/sync-from/other-id')
+        self.assertEqual(200, resp.status)
+        self.assertEqual('application/json', resp.header('content-type'))
+        self.assertEqual(dict(this_replica_uid='db0',
+                              this_replica_generation=0,
+                              other_replica_uid='other-id',
+                              other_replica_generation=1),
+                              json.loads(resp.body))
+
+    def test_record_sync_info(self):
+        resp = self.app.put('/db0/sync-from/other-id',
+                            params='{"generation": 2}',
+                            headers={'content-type': 'application/json'})
+        self.assertEqual(200, resp.status)
+        self.assertEqual('application/json', resp.header('content-type'))
+        self.assertEqual({'ok': True}, json.loads(resp.body))
+        self.assertEqual(self.db0.get_sync_generation('other-id'), 2)
+
+    def test_sync_exchange_send(self):
+        entry = {'id': 'doc-here', 'rev': 'replica:1', 'doc':
+                 '{"value": "here"}'}
+        args = dict(from_replica_generation=10, last_known_generation=0)
+        body = ("%s\r\n" % json.dumps(args) +
+                "%s\r\n" % json.dumps(entry))
+        resp = self.app.post('/db0/sync-from/replica',
+                            params=body,
+                            headers={'content-type':
+                                     'application/x-u1db-multi-json'})
+        self.assertEqual(200, resp.status)
+        self.assertEqual('application/x-u1db-multi-json',
+                         resp.header('content-type'))
+        self.assertEqual({'new_generation': 1}, json.loads(resp.body))
+        self.assertEqual(('replica:1', '{"value": "here"}', False),
+                         self.db0.get_doc('doc-here'))
+
+    def test_sync_exchange_receive(self):
+        doc_id, doc_rev = self.db0.create_doc('{"value": "there"}')
+        args = dict(from_replica_generation=10, last_known_generation=0)
+        body = "%s\r\n" % json.dumps(args)
+        resp = self.app.post('/db0/sync-from/replica',
+                            params=body,
+                            headers={'content-type':
+                                     'application/x-u1db-multi-json'})
+        self.assertEqual(200, resp.status)
+        self.assertEqual('application/x-u1db-multi-json',
+                         resp.header('content-type'))
+        parts = resp.body.splitlines()
+        self.assertEqual(2, len(parts))
+        self.assertEqual({'new_generation': 1}, json.loads(parts[0]))
+        self.assertEqual({'doc': '{"value": "there"}',
+                          'rev': doc_rev, 'id': doc_id}, json.loads(parts[1]))
