b8e6c8a87cf0ec203f4f90b7e8f3e9e6a996b4dd
John Arbash Meinel <john@arbash-meinel.com>
2011-11-30 14:14:11 +0100

    A bit more documentation.

--- u1db/__init__.py
+++ u1db/__init__.py
@@ -21,6 +21,9 @@ __version__ = '.'.join(map(str, __version_info__))
 def open(path, create):
     """Open a database at the given location.
 
+    Will raise u1db.errors.DatabaseDoesNotExist if create=False and the
+    database does not already exist.
+
     :param path: The filesystem path for the database to open.
     :param create: True/False, should the database be created if it doesn't
         already exist?
