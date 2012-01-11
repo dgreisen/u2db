499f697372a55086c7978969df8cf8297325fe0a
John Arbash Meinel <john@arbash-meinel.com>
2012-01-11 15:39:45 +0100

    Move across the direct CDatabase tests.

--- u1db/tests/test_c_backend.py
+++ u1db/tests/test_c_backend.py
@@ -22,11 +22,34 @@ except ImportError:
     c_backend_wrapper = None
 
 
-class TestCDatabase(tests.TestCase):
-
-    def test_exists(self):
-        if c_backend_wrapper is None:
+if c_backend_wrapper is None:
+    # Rather than failing all the related tests, just add one test that
+    # indicates the backend isn't available
+    class NoCDatabase(tests.TestCase):
+        def test_exists(self):
             self.fail("Could not import the c_backend_wrapper module."
                       " Was it compiled properly?")
+else:
+    class TestCDatabase(tests.TestCase):
+
+        def test_exists(self):
+            if c_backend_wrapper is None:
+                self.fail("Could not import the c_backend_wrapper module."
+                          " Was it compiled properly?")
+            db = c_backend_wrapper.CDatabase(':memory:')
+            self.assertEqual(':memory:', db._filename)
+
+        def test__is_closed(self):
+            db = c_backend_wrapper.CDatabase(':memory:')
+            self.assertTrue(db._sql_is_open())
+            db._close_sqlite_handle()
+            self.assertFalse(db._sql_is_open())
+
+        def test__run_sql(self):
+            db = c_backend_wrapper.CDatabase(':memory:')
+            self.assertTrue(db._sql_is_open())
+            self.assertEqual([], db._run_sql('CREATE TABLE test (id INTEGER)'))
+            self.assertEqual([], db._run_sql('INSERT INTO test VALUES (1)'))
+            self.assertEqual([('1',)], db._run_sql('SELECT * FROM test'))
 
 
