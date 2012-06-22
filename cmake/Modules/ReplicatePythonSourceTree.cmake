441f8d01da15560eaebab03a17baf4c134fa3a35
Roberto Alsina <roberto.alsina@canonical.com>
2012-06-22 17:55:39 -0300

    copy more files

--- cmake/Modules/ReplicatePythonSourceTree.cmake
+++ cmake/Modules/ReplicatePythonSourceTree.cmake
@@ -1,5 +1,6 @@
 # Note: when executed in the build dir, then CMAKE_CURRENT_SOURCE_DIR is the
 # build dir.
+file( COPY html-docs DESTINATION "${CMAKE_ARGV3}")
 file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
   FILES_MATCHING PATTERN "*.py" )
 file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
@@ -10,3 +11,6 @@ file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
   FILES_MATCHING PATTERN "*.key" )
 file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
   FILES_MATCHING PATTERN "*.cert" )
+file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
+  FILES_MATCHING PATTERN "*.pem" )
+file( COPY "u1db-serve" "u1db-client" "MANIFEST.in" "README" "COPYING" "COPYING.LESSER" DESTINATION "${CMAKE_ARGV3}")
