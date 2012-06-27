c82de9b9a723f9303189580b5c24863413858337
Roberto Alsina <roberto.alsina@canonical.com>
2012-06-27 13:53:06 -0300

    nicer syntax

--- cmake/Modules/ReplicatePythonSourceTree.cmake
+++ cmake/Modules/ReplicatePythonSourceTree.cmake
@@ -2,15 +2,13 @@
 # build dir.
 file( COPY html-docs DESTINATION "${CMAKE_ARGV3}")
 file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
-  FILES_MATCHING PATTERN "*.py" )
-file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
-  FILES_MATCHING PATTERN "*.pyx" )
-file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
-  FILES_MATCHING PATTERN "*.sql" )
-file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
-  FILES_MATCHING PATTERN "*.key" )
-file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
-  FILES_MATCHING PATTERN "*.cert" )
-file( COPY setup.py u1db  DESTINATION "${CMAKE_ARGV3}"
-  FILES_MATCHING PATTERN "*.pem" )
-file( COPY "u1db-serve" "u1db-client" "MANIFEST.in" "README" "COPYING" "COPYING.LESSER" DESTINATION "${CMAKE_ARGV3}")
+  FILES_MATCHING
+  PATTERN "*.py"
+  PATTERN "*.pyx"
+  PATTERN "*.pyx"
+  PATTERN "*.sql"
+  PATTERN "*.key"
+  PATTERN "*.cert"
+  PATTERN "*.pem" )
+file( COPY "u1db-serve" "u1db-client" "MANIFEST.in" "README"
+    "COPYING" "COPYING.LESSER" DESTINATION "${CMAKE_ARGV3}")
